5c143f2fe06e8f1c41ebc6af5d9aa62f735a2e8a
ch1ch0gz <ch1ch0gz@ch1ch0gzs-Air.cust.communityfibre.co.uk>
2023-07-12 11:53:00 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import './App.css';
 import Safe from './components/Safe'
 import SafesList from './components/SafesList'
 import TransactionsList from './components/TransactionsList';
+import CreateSafe from './components/CreateSafe ';
+
 
 function App() {
   return (
+
     <div className="p-20 flex flex-col gap-6 bg-[#1C1C1C]">
         <Safe safeAddr="0x6106FB94E31B83D0A15432FCA2927B838fB6D025" apiUrl="https://safe-transaction-gnosis-chain.safe.global/" chainNativeToken={{symbol: "xDai", icon: "https://s2.coinmarketcap.com/static/img/coins/64x64/8635.png", decimals: 18}}></Safe>
         <SafesList walletAddr="0x0E1774FD4f836E6Ba2E22d0e11F4c69684ae4EB7" apiUrl="https://safe-transaction-gnosis-chain.safe.global/"></SafesList>
         <TransactionsList safeAddr="0x6106FB94E31B83D0A15432FCA2927B838fB6D025" apiUrl="https://safe-transaction-gnosis-chain.safe.global/"></TransactionsList>
+        <CreateSafe></CreateSafe>
     </div>
+
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,82 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+import './styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme, lightTheme, midnightTheme,
+} from '@rainbow-me/rainbowkit';  
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'; 
+
+
+
+const GnosisChain = {
+  id: 100,
+  name: 'Gnosis Chain',
+  network: 'Gnosis',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'xDai',
+    symbol: 'xDai',
+  },
+  rpcUrls: {
+    default: 'https://rpc.ankr.com/gnosis',
+  },
+  blockExplorers: {
+    default: { name: 'Blockscout', url: 'https://blockscout.com/xdai/mainnet' },
+  },
+  iconUrls: ["https://images.prismic.io/koinly-marketing/16d1deb7-e71f-48a5-9ee7-83eb0f7038e4_Gnosis+Chain+Logo.png"],
+  testnet: false,
+}
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, GnosisChain],
+  [
+    alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
+    jsonRpcProvider({ rpc: () => ({ http: "https://rpc.ankr.com/gnosis" }) }), 
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={wagmiClient}>
+    <RainbowKitProvider coolMode chains={chains} theme={lightTheme({
+      accentColor: '#623485', 
+      accentColorForeground: 'white', 
+      borderRadius: 'large',
+      fontStack: 'system',  
+    })}>
+      <ConnectButton showBalance={false} />
+      <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>
 );
 
