f455f1cd998e6840ffc1f56a1deb6c73cbab7cf7
ch1ch0gz <66129281+ch1ch0gz@users.noreply.github.com>
2023-08-06 15:13:26 +0100

    Create chainAPIConfigFile.jsx
    gnosis api addresses per chain

---
+++ NearBOS Widgets/chainAPIConfigFile.js
@@ -0,0 +1,56 @@
+{
+  "42161": {
+    "chainName": "Arbitrum",
+    "apiBaseUrl": "https://safe-transaction-arbitrum.safe.global",
+    "chainNativeToken": {
+      "icon": "https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png",
+      "symbol": "ETH",
+      "decimals": 18
+    }
+  },
+  "43114": {
+    "chainName": "Avalanche",
+    "apiBaseUrl": "https://safe-transaction-avalanche.safe.global",
+    "chainNativeToken": {
+      "icon": "https://s2.coinmarketcap.com/static/img/coins/64x64/5805.png",
+      "symbol": "AVAX",
+      "decimals": 18
+    }
+  },
+  "56": {
+    "chainName": "BNB",
+    "apiBaseUrl": "https://safe-transaction-bsc.safe.global",
+    "chainNativeToken": {
+      "icon": "https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png",
+      "symbol": "BNB",
+      "decimals": 18
+    }
+  },
+  "1": {
+    "chainName": "ETH",
+    "apiBaseUrl": "https://safe-transaction-mainnet.safe.global",
+    "chainNativeToken": {
+      "icon": "https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png",
+      "symbol": "ETH",
+      "decimals": 18
+    }
+  },
+  "100": {
+    "chainName": "Gnosis",
+    "apiBaseUrl": "https://safe-transaction-gnosis-chain.safe.global/",
+    "chainNativeToken": {
+      "icon": "https://s2.coinmarketcap.com/static/img/coins/64x64/8635.png",
+      "symbol": "xDAI",
+      "decimals": 18
+    }
+  },
+  "10": {
+    "chainName": "Optimism",
+    "apiBaseUrl": "https://safe-transaction-gnosis-chain.safe.global/",
+    "chainNativeToken": {
+      "icon": "https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png",
+      "symbol": "ETH",
+      "decimals": 18
+    }
+  }
+}
